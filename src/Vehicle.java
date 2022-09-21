f9e33d1dfaea3ee6fd3b28194f47fe8c01e4ac0c
Philip Andersson <philip.andersson.1991@hotmail.com>
2022-09-21 21:17:55 +0200

    3rd itteration (cleaned up setters/getters)

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -18,50 +18,16 @@ public class Vehicle {
 
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Driver getDriver() {
-        return driver;
-    }
     //setter för kompositionen Driver
     public void setDriver(Driver driver) {
         this.driver = driver;
         // (if sats) om Driver;age är större eller = Vehicle(Car/tank) legalAge
-        // är setDriver giltligt och null bytas ut mot giltlig Driver
+        // är setDriver giltigt och null bytas ut mot giltig Driver
         if (driver.getAge() >= legalAge)
             System.out.println("Driver changed to " + driver.getName());
         else System.out.println("Driver not changed! " + driver.getName() + " is " + driver.getAge() + ", but must be " + legalAge + " or older to drive car");
     }
 
-    public int getLegalAge() {
-        return legalAge;
-    }
-
-    public void setLegalAge(int legalAge) {
-        this.legalAge = legalAge;
-    }
-
-    public int getMilesToGo() {
-        return milesToGo;
-    }
-
-    public void setMilesToGo(int milesToGo) {
-        this.milesToGo = milesToGo;
-    }
-
-    public int getMilesPerDrive() {
-        return milesPerDrive;
-    }
-
-    public void setMilesPerDrive(int milesPerDrive) {
-        this.milesPerDrive = milesPerDrive;
-    }
     // Metod drive som kollar utifall Driver är null.
     // Komposition till: (Vehicle (car/tank)
     public void drive () {
